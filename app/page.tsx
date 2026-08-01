b8b48d235f4847c1eab6554b222b89c0a202cbbc
JamesWhiteley <whiteley6990@hotmail.com>
2026-08-01 09:19:38 +0100

    fix: pass required userName prop to AuraJournal

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import AuraJournal from "@/components/aura-journal"
+
+export default function Page() {
+  return <AuraJournal userName="User" />
+}
 import { AuraJournal } from '@/components/aura-journal'
 
 export default function Page() {
